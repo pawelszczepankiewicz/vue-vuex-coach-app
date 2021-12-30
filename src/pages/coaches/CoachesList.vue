5e41db3563c1e90ae4865005a21b8e66da47a652
Paweł Szczepankiewicz <pawel.szczepankiewicz@outlook.com>
2021-12-30 15:10:12 +0100

    Added authentication

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,29 +1,47 @@
 <template>
-  <section>
-    <coach-filter @change-filter="setFilters"></coach-filter>
-  </section>
-  <section>
-    <base-card>
-      <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
-        <base-button v-if="!isCoach" link to="/register"
-          >Register as Coach</base-button
-        >
-      </div>
-      <ul v-if="hasCoaches">
-        <coach-item
-          v-for="coach in filteredCoaches"
-          :key="coach.id"
-          :id="coach.id"
-          :first-name="coach.firstName"
-          :last-name="coach.lastName"
-          :rate="coach.hourlyRate"
-          :areas="coach.areas"
-        ></coach-item>
-      </ul>
-      <h3 v-else>No coaches found</h3>
-    </base-card>
-  </section>
+  <div>
+    <base-dialog :show="!!error" title="An error occured!" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <section>
+      <coach-filter @change-filter="setFilters"></coach-filter>
+    </section>
+    <section>
+      <base-card>
+        <div class="controls">
+          <base-button mode="outline" @click="loadCoaches(true)"
+            >Refresh</base-button
+          >
+          <base-button
+            link
+            to="/auth?redirect=register"
+            v-if="!isLoggedIn"
+          ></base-button>
+          <base-button
+            v-if="isLoggedIn && !isCoach && !isLoading"
+            link
+            to="/register"
+            >Login to register as Coach</base-button
+          >
+        </div>
+        <div v-if="isLoading">
+          <base-spinner></base-spinner>
+        </div>
+        <ul v-else-if="hasCoaches">
+          <coach-item
+            v-for="coach in filteredCoaches"
+            :key="coach.id"
+            :id="coach.id"
+            :first-name="coach.firstName"
+            :last-name="coach.lastName"
+            :rate="coach.hourlyRate"
+            :areas="coach.areas"
+          ></coach-item>
+        </ul>
+        <h3 v-else>No coaches found</h3>
+      </base-card>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -34,6 +52,8 @@ export default {
   components: { CoachItem, CoachFilter },
   data() {
     return {
+      isLoading: false,
+      error: null,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -42,6 +62,9 @@ export default {
     };
   },
   computed: {
+    isLoggedIn() {
+      return this.$store.getters.isAuthenticated;
+    },
     isCoach() {
       return this.$store.getters['coaches/isCoach'];
     },
@@ -61,12 +84,29 @@ export default {
       });
     },
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches'];
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
     },
+  },
+  created() {
+    this.loadCoaches();
   },
   methods: {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
+    },
+    async loadCoaches(refresh = false) {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('coaches/loadCoaches', {
+          forceRefresh: refresh,
+        });
+      } catch (error) {
+        this.error = error.message || 'Something went wrong!';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     },
   },
 };
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,49 @@
 export default {
-    contactCoach(context, payload) {
+    async contactCoach(context, payload) {
         const newRequest = {
-            id: new Date().toISOString(),
-            coachId: payload.coachId,
             userEmail: payload.email,
             message: payload.message
+        };
+        const response = await fetch(`some-url/requests/${payload.coachId}.json`, {
+            method: 'POST',
+            body: JSON.stringify(newRequest)
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(response.message || 'Failed to send request')
+            throw error
         }
-        context.commit('addRequest', newRequest)
+
+        newRequest.id = responseData.name;
+        newRequest.coachId = payload.coachId;
+
+        context.commit('addRequest', newRequest);
+    },
+    async fetchRequests(context) {
+        const coachId = context.rootGetters.userId
+        const token = context.rootGetters.token
+        const response = await fetch(`some-url/requests/${coachId}.json?auth=${token}`)
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            const error = new Error(response.message || 'Failed to fetch requests')
+            throw error
+        }
+
+        const requests = [];
+
+        for (const key in responseData) {
+            const request = {
+                id: key,
+                coachId: coachId,
+                userEmail: responseData[key].userEmail,
+                message: responseData[key].message,
+            }
+            requests.push(request);
+        }
+
+        context.commit('setRequests', requests)
     }
 }
